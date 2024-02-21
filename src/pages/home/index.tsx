4166d3f42748a155080d6f561176d7a30e9ba5a1
G.J <gjconsultoriati@gmail.com>
2024-02-21 15:00:40 -0300

    feat: Add SEO

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,26 +3,34 @@ import { Container, Hero, Preview } from './styles'
 import previewImage from '@/assets/app-preview.png'
 import Image from 'next/image'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
+
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size="4xl">Agendameto descomplicado</Heading>
-        <Text size="xl">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
-        <ClaimUsernameForm />
-      </Hero>
-      <Preview>
-        <Image
-          src={previewImage}
-          height={400}
-          quality={100}
-          priority
-          alt="Calendario simbolizando aplicação funcionando"
-        />
-      </Preview>
-    </Container>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Web Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre"
+      />
+      <Container>
+        <Hero>
+          <Heading size="4xl">Agendameto descomplicado</Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+          <ClaimUsernameForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage}
+            height={400}
+            quality={100}
+            priority
+            alt="Calendario simbolizando aplicação funcionando"
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -4,6 +4,7 @@ import { AuthError, ConnectBox, ConnectItem } from './styles'
 import { ArrowRight, Check } from 'phosphor-react'
 import { signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import { NextSeo } from 'next-seo'
 
 export default function ConnectCalendar() {
   const router = useRouter()
@@ -19,48 +20,52 @@ export default function ConnectCalendar() {
   async function handleNavigateToNextStep() {
     await router.push('/register/time-intervals')
   }
+
   return (
-    <Container>
-      <Header>
-        <Heading as="strong">Conecte sua agenda!</Heading>
-        <Text>
-          Conecte o seu calendário para verificar automaticamente as horas
-          ocupadas e os novos eventos à medida em que são agendados.
-        </Text>
+    <>
+      <NextSeo title="Conecte sua agenda do google | Web Call" noindex />
+      <Container>
+        <Header>
+          <Heading as="strong">Conecte sua agenda!</Heading>
+          <Text>
+            Conecte o seu calendário para verificar automaticamente as horas
+            ocupadas e os novos eventos à medida em que são agendados.
+          </Text>
 
-        <MultiStep size={4} currentStep={2} />
-      </Header>
+          <MultiStep size={4} currentStep={2} />
+        </Header>
 
-      <ConnectBox>
-        <ConnectItem>
-          <Text>Google Calendar</Text>
-          {isSignedId ? (
-            <Button size="sm" disabled>
-              Conectado
-              <Check />
-            </Button>
-          ) : (
-            <Button variant="secondary" size="sm" onClick={handleSignIn}>
-              Conectar
-              <ArrowRight />
-            </Button>
+        <ConnectBox>
+          <ConnectItem>
+            <Text>Google Calendar</Text>
+            {isSignedId ? (
+              <Button size="sm" disabled>
+                Conectado
+                <Check />
+              </Button>
+            ) : (
+              <Button variant="secondary" size="sm" onClick={handleSignIn}>
+                Conectar
+                <ArrowRight />
+              </Button>
+            )}
+          </ConnectItem>
+          {hasAuthError && (
+            <AuthError size="sm">
+              Falha ao se conectar ao Google, verifique se você habilitou as
+              permissões de acesso ao Google Calendar
+            </AuthError>
           )}
-        </ConnectItem>
-        {hasAuthError && (
-          <AuthError size="sm">
-            Falha ao se conectar ao Google, verifique se você habilitou as
-            permissões de acesso ao Google Calendar
-          </AuthError>
-        )}
-        <Button
-          type="submit"
-          disable={!isSignedId}
-          onClick={handleNavigateToNextStep}
-        >
-          Próximo passo
-          <ArrowRight />
-        </Button>
-      </ConnectBox>
-    </Container>
+          <Button
+            type="submit"
+            disable={!isSignedId}
+            onClick={handleNavigateToNextStep}
+          >
+            Próximo passo
+            <ArrowRight />
+          </Button>
+        </ConnectBox>
+      </Container>
+    </>
   )
 }
